629898ce82455e8c7ca191506d7a057218fc4aa0
q06mrashid-sketch <q06mrashid@googlemail.com>
2025-08-20 16:11:54 +0100

    feat: add subscription management screen

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -7,6 +7,7 @@ import SwipeTabs from './SwipeTabs';
 import MembershipInfoScreen from '../screens/MembershipInfoScreen';
 import MembershipStartScreen from '../screens/MembershipStartScreen';
 import LoyaltyCardCreateScreen from '../screens/LoyaltyCardCreateScreen';
+import ManageSubscriptionScreen from '../screens/ManageSubscriptionScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,7 @@ export default function Router() {
         <Stack.Screen name="MainTabs" component={SwipeTabs} />
         <Stack.Screen name="MembershipInfo" component={MembershipInfoScreen} />
         <Stack.Screen name="MembershipStart" component={MembershipStartScreen} />
+        <Stack.Screen name="ManageSubscription" component={ManageSubscriptionScreen} />
         <Stack.Screen name="LoyaltyCardCreate" component={LoyaltyCardCreateScreen} />
       </Stack.Navigator>
     </NavigationContainer>
